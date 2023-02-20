7c418f37885c53c07ac5424533d55e9ba467832d
JonDev2023 <roupasee@gmail.com>
2023-02-20 19:14:59 -0300

    .

---
+++ run.bat
@@ -0,0 +1,37 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWHreyHcjLQlHcBKGLn63OoUF6+D6/ee3sEIXUeEra7Px1bqPLqAw60nhe5IsxDpOl4shAxRWQh+qZwF6uXci
+::fBE1pAF6MU+EWHreyHcjLQlHcBKGLn63OoUF6+D6/ee3sEIXUeEra7Px1bqPLqAw60nhe5IsxDpOl4shAxRWQhikYA46lm1DumGLM8aOjBvudUWG6UV+EHMU
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSTk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQI7IQ1NLA==
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFA1QXhGPAFujBb0d+uXL/eWNp04JR94Vd4Ha1PStIewW5EHlZ9Y52Tp3l8QCMDFXcBOlLiw1pWtMsmWXd9OT8yLuRUbH41M1ew==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@ECHO OFF
+python main.py
